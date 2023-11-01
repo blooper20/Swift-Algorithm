175d9b05c2dad3b60cd5b73c993db75b5cc14878
DwaeWoo <jaewoo@DwaeWooui-MacBookAir.local>
2023-11-01 14:09:46 +0900

    수정

---
+++ 나머지가 1이 되는 수 찾기.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    var answer: Int = 0
+    
+    for i in 1...n {
+        if n % i == 1 {
+            answer = i
+            break
+        }
+    }
+    
+    return answer
+}
---
+++ 문자열을 정수로 바꾸기.swift
@@ -0,0 +1,4 @@
+func solution(_ s:String) -> Int {
+    guard let answer = Int(s) else { return 0 }
+    return answer
+}
